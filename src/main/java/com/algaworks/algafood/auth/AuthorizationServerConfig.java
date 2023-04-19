434608b7a75f18c3fe4aa69cdb6476c9ef4f63fe
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-04-19 16:20:05 -0300

    Configurando o fluxo Authorization Server com Password Credentials e Opaque Tokens

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -1,11 +1,44 @@
 package com.algaworks.algafood.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+//	Esses dados devem ser passados em "Authorization" dentro do Postman. São os dados do cliente.
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+					.withClient("algafood-web")					//Cliente Web
+					.secret(passwordEncoder.encode("web123"))
+					.authorizedGrantTypes("password")			//Fluxo de Password
+					.scopes("write", "read")
+					.accessTokenValiditySeconds(60 * 60 * 6);	//Tempo de expiração do token (em segundos). Neste caso configurado em 6hs
+//				.and()
+//					.withClient("app-mobile")					//Cliente Mobile
+//					.secret(passwordEncoder.encode("mob123"))	
+//					.authorizedGrantTypes("password", "outroGrandType")			//Fluxo de Password
+//					.scopes("write", "read");
+	}
 
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager);
+	}
+	
 }
--- src/main/java/com/algaworks/algafood/auth/WebSecurityConfig.java
+++ src/main/java/com/algaworks/algafood/auth/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +15,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+//	Esses dados devem ser passados no Body da requisiçao. São os dados do Resource Owner
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -31,5 +34,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
 	}
 }

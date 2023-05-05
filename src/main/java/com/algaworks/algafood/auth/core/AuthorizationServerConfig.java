22a3fc983ab475a2ea595c8e42635648adf86035
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-05-05 17:23:59 -0300

    Configurando os clientes OAuth2 em um banco de dados SQL

--- src/main/java/com/algaworks/algafood/auth/core/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/core/AuthorizationServerConfig.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood.auth.core;
 
 import java.util.Arrays;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,9 +30,6 @@ import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFacto
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
 	
 	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
-	@Autowired
 	private AuthenticationManager authenticationManager;
 	
 //	Refresh token precisa deste service
@@ -40,44 +39,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private JwtKeyStoreProperties jwtKeyStoreProperties; 
 
+//	Fonte de dados
+	@Autowired
+	private DataSource dataSource;
+	
 //	Esses dados devem ser passados em "Authorization" dentro do Postman. São os dados do cliente.
 	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory()
-					.withClient("algafood-web")							//Cliente Web
-					.secret(passwordEncoder.encode("web123"))
-					.authorizedGrantTypes("password", "refresh_token")	//Fluxo de Password / Refresh Token (Por padrão, expira em 30 Dias)
-					.scopes("WRITE", "READ")
-					.accessTokenValiditySeconds(60 * 60 * 6)			//6 horas
-					.refreshTokenValiditySeconds(60 * 24 * 60 * 60)		//60 dias				
-				.and()
-					.withClient("faturamento")							//Cliente de aplicacao backend  que irá consultar a API
-					.secret(passwordEncoder.encode("faturamento123"))
-					.authorizedGrantTypes("client_credentials")
-					.scopes("WRITE", "READ")
-					
-//	Link para acessar no navegador: http://auth.algafood.local:8081/oauth/authorize?response_type=code&client_id=foodanalytics&state=abc&redirect_uri=http://aplicacao-cliente
-//	Irá aparecer a tela para logar, e em seguida autorizar os acessos do cliente "foodanalytics", depois de autorizar, ele irá gera o "code" que será utilizado
-//	para solicitar um "AcessToken" para então poder utilizar a API AlgaFood
-	
-//	Link para utilzar PKCE com metodo "Plain"
-//	http://auth.algafood.local:8081/oauth/authorize?response_type=code&client_id=foodanalytics&direct_uri=http://localhost:8082&
-//	code_challenge=teste123&code_challenge_method=plain
-				.and()
-					.withClient("foodanalytics")
-					.secret(passwordEncoder.encode(""))		//Com o PKCE não é obrigatorio o uso de um "secret"	
-					.authorizedGrantTypes("authorization_code")
-					.scopes("WRITE", "READ")
-					.redirectUris("http://localhost:8082")	//Não tem a barra no final como tinha no ultimo commit antes deste
-				.and()									 	//Exemplo luxo Implicit Grant Type (não recomendado pois o token vai na URI)
-					.withClient("webadmin")
-					.authorizedGrantTypes("implicit")
-					.scopes("WRITE", "READ")
-					.redirectUris("http://aplicacao-cliente")
-				.and()
-					.withClient("checktoken")
-						.secret(passwordEncoder.encode("check123"));
+		clients.jdbc(dataSource);
 	}
 
 	@Override
